8fe8269f853c8d4e5d60c3dbcef6513e07aa6d3a
Maxwell Pray <synthead@gmail.com>
2022-08-13 04:10:39 -0700

    Use 0x00 for name attribute file position.

--- lib/god_of_thunder_save.rb
+++ lib/god_of_thunder_save.rb
@@ -6,7 +6,7 @@ require "god_of_thunder_save/string_value"
 
 class GodOfThunderSave
   ENTRIES = {
-    name: StringValue.new(pos: 0, length: 22),
+    name: StringValue.new(pos: 0x00, length: 22),
     health: IntegerValue.new(pos: 0x63, bytes: 1),
     magic: IntegerValue.new(pos: 0x64, bytes: 1),
     jewels: IntegerValue.new(pos: 0x65, bytes: 2),
