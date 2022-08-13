717f4855b9520cd954f2a219a2e7ca8cf7415d63
Maxwell Pray <synthead@gmail.com>
2022-08-13 02:58:13 -0700

    Reorder ENTRIES so #attributes looks nice.

--- lib/god_of_thunder_save.rb
+++ lib/god_of_thunder_save.rb
@@ -9,13 +9,13 @@ class GodOfThunderSave
     magic: IntegerValue.new(pos: 0x64, bytes: 1),
     jewels: IntegerValue.new(pos: 0x65, bytes: 2),
     keys: IntegerValue.new(pos: 0x67, bytes: 1),
+    score: IntegerValue.new(pos: 0x70, bytes: 4),
     enchanted_apple: BitmaskValue.new(pos: 0x69, bitmask: 0x01),
     lightning_power: BitmaskValue.new(pos: 0x69, bitmask: 0x02),
     winged_boots: BitmaskValue.new(pos: 0x69, bitmask: 0x04),
     wind_power: BitmaskValue.new(pos: 0x69, bitmask: 0x08),
     amulet_of_protection: BitmaskValue.new(pos: 0x69, bitmask: 0x10),
-    thunder_power: BitmaskValue.new(pos: 0x69, bitmask: 0x20),
-    score: IntegerValue.new(pos: 0x70, bytes: 4)
+    thunder_power: BitmaskValue.new(pos: 0x69, bitmask: 0x20)
   }.freeze
 
   attr_reader :path
